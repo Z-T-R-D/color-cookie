07d3b4a731bd44ead66c0c42297a16899c200f9e
emmanuel <emmanuelforchinagorom@gmail.com>
2024-06-05 04:19:51 +0100

    added tailwind ,assets  and created search component

---
+++ src/components/search.tsx
@@ -0,0 +1,39 @@
+import { ReactSVG } from "react-svg";
+import Icon from "../assets/search.svg";
+import { useState } from "react";
+
+export const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSearch = () => {
+    if (!searchValue) return;
+    console.log("searching for", searchValue);
+    setSearchValue("");
+
+  }
+
+  const containerClasses =
+    "group flex items-center justify-center focus-within:ring-blue-500 p-2 border-2 border-neutral-400 rounded-md focus-within:ring-2 focus-within:border-transparent w-4/5 sm:w-[450px] bg-white";
+  const inputClasses =
+    "focus:outline-none text-md text-neutral-900 placeholder-neutral-200 font-semibold pr-2 pl-1 w-full bg-transparent";
+  const svgClasses = `text-neutral-400 w-6 cursor-pointer group-focus-within:text-blue-500 ${
+    searchValue ? "active:text-neutral-200" : "text-neutral-200"
+  }`;
+
+  return (
+    <div className={containerClasses}>
+      <input
+        type="search"
+        placeholder=" search color name or hex"
+        className={inputClasses}
+        value={searchValue}
+        onChange={handleInputChange}
+      />
+      <ReactSVG src={Icon} className={svgClasses} onClick={handleSearch}/>
+    </div>
+  );
+};
