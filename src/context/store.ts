ee9d66bc81089ebb1705e770cf26d4d487a77352
emmanuel <emmanuelforchinagorom@gmail.com>
2024-06-07 02:08:16 +0100

    used zusted to create a store for global data

--- src/context/store.ts
+++ src/context/store.ts
@@ -1,13 +1,22 @@
 import { create } from "zustand";
 
+type ComplimentaryColors = [string, string];
+
 type Store = {
-  isChecked: boolean;
+  currentColor: string;
+  complimentaryColors: ComplimentaryColors;
   darkMode: boolean;
-  toggle: () => void;
+  errorMessage: string;
+  show: boolean;
+  toggleShow: () => void;
+  addErrorMessage: (msg: string) => void;
+  addCurrentColor: (clr: string) => void;
+  addComplimentaryColors: (clrs: ComplimentaryColors) => void;
   toggleDarkMode: () => void;
 };
 
-const AddDarkMode = () => {
+// add dark mode to the root element
+const AddDarkModeToRoot = () => {
   const rootElement = document.getElementById("root");
   if (rootElement) {
     rootElement.classList.toggle("dark");
@@ -15,12 +24,19 @@ const AddDarkMode = () => {
 };
 
 export const useStore = create<Store>((set) => ({
-  isChecked: false,
-  darkMode: true,
-  toggle: () => set((state) => ({ isChecked: !state.isChecked })),
+  currentColor: "",
+  complimentaryColors: ["", ""],
+  darkMode: false,
+  errorMessage: "",
+  show: false,
+  toggleShow: () => set((state) => ({ show: !state.show })),
+  addErrorMessage: (msg: string) => set(() => ({ errorMessage: msg })),
+  addCurrentColor: (clr: string) => set(() => ({ currentColor: clr })),
+  addComplimentaryColors: (clrs: ComplimentaryColors) =>
+    set(() => ({ complimentaryColors: clrs })),
   toggleDarkMode: () =>
     set((state) => {
-      AddDarkMode();
+      AddDarkModeToRoot();
       return { darkMode: !state.darkMode };
     }),
 }));
